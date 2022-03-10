7671fd46a012a9f5e0850c66e31096a1ed9b279e
emsthf <deunsol916@gmail.com>
2022-03-10 16:21:19 +0900

    pipeline test2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 테스트
+          Learn React 테스트2!!!!!!!!!!!!!!!!!!!
         </a>
       </header>
     </div>
