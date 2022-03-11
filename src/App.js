c92320c40a965927cd19cf0ffcf9fcee4750a6e4
emsthf <deunsol916@gmail.com>
2022-03-11 09:53:02 +0900

    slack alarm test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 테스트2!!!!!!!!!!!!!!!!!!!
+          Learn React 테스트333!!!!!!!!!!!!!!!!!!!
         </a>
       </header>
     </div>
