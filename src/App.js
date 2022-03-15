a057e3b47c4a99dc63511b5d0443b2206efe6779
rivera00255 <rivera00255@gmail.com>
2022-03-15 12:34:32 +0900

    edit style and components

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import DashBoard from "./pages/DashBoard";
 import GoalDetail from "./pages/GoalDetail";
-import SetGoalStep1 from "./pages/SetGoalStep1";
-import SetGoalStep2 from "./pages/SetGoalStep2";
-import SetGoalStep3 from "./pages/SetGoalStep3";
-import SetGoalStep4 from "./pages/SetGoalStep4";
-import SetGoalStep5 from "./pages/SetGoalStep5";
+import SetGoal from "./pages/SetGoal";
 
 function App() {
   return (
@@ -15,11 +11,11 @@ function App() {
       <Header />
       <Routes>
         <Route path="/dash" element={<DashBoard />} />
-        <Route path="/" element={<SetGoalStep1 />} />
-        <Route path="/set/2" element={<SetGoalStep2 />} />
-        <Route path="/set/3" element={<SetGoalStep3 />} />
-        <Route path="/set/4" element={<SetGoalStep4 />} />
-        <Route path="/set/5" element={<SetGoalStep5 />} />
+        <Route path="/" element={<SetGoal step='1' />} />
+        <Route path="/set/2" element={<SetGoal step='2' />} />
+        <Route path="/set/3" element={<SetGoal step='3' />} />
+        <Route path="/set/4" element={<SetGoal step='4' />} />
+        <Route path="/set/5" element={<SetGoal step='5' />} />
         <Route path="/goal" element={<GoalDetail />} />
       </Routes>
       <Footer />
--- src/pages/GoalDetail.js
+++ src/pages/GoalDetail.js
@@ -1,100 +1,124 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import styled from "styled-components";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 const Container = styled.div`
-  width: 1200px;
-  margin: 0 auto;
+width: 1200px;
+margin: 20vh auto;
+min-height: 100vh;
 `;
 
 const Wrapper = styled.div`
-  width: 100%;
+width: 100%;
 `;
 
 const Title = styled.div`
-  width: 800px;
-  height: 100px;
-  border-radius: 20px;
-  background: #fad390;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin: 1rem auto;
+width: 800px;
+height: 100px;
+background: #fafafa;
+border-radius: 20px;
+box-shadow: 3px 5px 10px #d7d7d7;
+display: flex;
+align-items: center;
+justify-content: center;
+margin: 2rem auto;
 `;
 
-const Desc = styled(Title)``;
+const Desc = styled(Title)`
+`;
 
-const Date = styled(Title)``;
+const Date = styled(Title)`
+`;
 
 const Check = styled.form`
-  width: 400px;
-  height: 80px;
-  margin: 0 auto;
-  border-radius: 20px;
-  background: #fad390;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+width: 400px;
+height: 80px;
+margin: 0 auto;
+background: #fafafa;
+border-radius: 20px;
+box-shadow: 3px 5px 10px #d7d7d7;
+display: flex;
+justify-content: center;
+align-items: center;
 `;
 
 const Input = styled.input`
-  margin-left: 1rem;
-  width: 1rem;
-  height: 1rem;
+margin-left: 1rem;
+width: 1rem;
+height: 1rem;
 `;
 
 const Button = styled.button`
-  padding: 0.2rem 1rem;
-  margin-left: ${(props) => props.marginLeft && "2rem"};
+padding: 0.5rem 3rem;
+border: none;
+box-shadow: 3px 4px 8px #b7b7b7;
+background: ${props => props.backgroundColor || '#416dea'};
+color: #fff;
+font-weight: bold;
+border-radius: 30px;
+margin: 1rem 0;
+margin-left: ${props => props.marginLeft && '2rem'};
+&:hover {
+    box-shadow: none;
+    background: ${props => props.hoverColor || 'linear-gradient(315deg, #89d8d3, #416dea 74%)'};
+}
+&:active {
+    box-shadow: none;
+    background: ${props => props.hoverColor || 'linear-gradient(315deg, #89d8d3, #416dea 74%)'};
+    box-shadow: 3px 4px 10px #bbb;
+}
 `;
 
 const ButtonWrapper = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  margin: 1rem 0;
+width: 100%;
+display: flex;
+justify-content: center;
+margin: 1rem 0;
 `;
 
 const Post = styled.div`
-  width: 80%;
-  margin: 0 auto;
-  border-top: 1px solid #000;
-  border-bottom: 1px solid #000;
-  box-sizing: border-box;
+width: 80%;
+margin: 0 auto;
+border-top: 1px solid #bbb;
+border-bottom: 1px solid #bbb;
+box-sizing: border-box;
 `;
 
-const StyledLink = styled.a`
-  padding: 0.2rem 2rem;
-  display: flex;
-  justify-content: space-between;
+const StyledLink = styled(Link)`
+padding: 0.5rem 2rem;
+display: flex;
+justify-content: space-between;
+&:hover {
+    background: #fafafa;
+}
 `;
 
 function GoalDetail() {
-  return (
-    <Container>
-      <Wrapper>
-        <Title>목표명</Title>
-        <Desc>설명</Desc>
-        <Date>기간</Date>
-        <Check>
-          <label>오늘의 목표 체크</label>
-          <Input type="checkbox"></Input>
-          <Button marginLeft>인증글쓰기</Button>
-          {/* <Progress></Progress>
+    return ( 
+        <Container>
+            <Wrapper>
+                <Title>목표명</Title>
+                <Desc>설명</Desc>
+                <Date>기간</Date>
+                <Check>
+                    <label>오늘의 목표 체크</label>
+                    <Input type='checkbox'></Input>
+                    <Button marginLeft>인증글 쓰기</Button>
+                    {/* <Progress></Progress>
                     <Chart></Chart> */}
-        </Check>
-        <ButtonWrapper>
-          <Button>포기</Button>
-        </ButtonWrapper>
-      </Wrapper>
-      <Post>
-        <StyledLink href="#">
-          <h4>post title</h4>
-          <p>post date</p>
-        </StyledLink>
-      </Post>
-    </Container>
-  );
+                </Check>
+                <ButtonWrapper>
+                    <Button backgroundColor={'#373737'} hoverColor={'linear-gradient(315deg, #8e8e8e, #373737 74%)'}>포 기</Button>
+                </ButtonWrapper>
+            </Wrapper>
+            <Post>
+                <StyledLink to='/'>
+                    <h4>post title</h4>
+                    <p>post date</p>
+                </StyledLink>
+            </Post>
+        </Container>
+     );
 }
 
 export default GoalDetail;
