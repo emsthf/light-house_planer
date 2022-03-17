71b129218ece3533b33b362d56bdc19d05080491
rivera00255 <rivera00255@gmail.com>
2022-03-17 12:50:26 +0900

    edit signup

--- src/pages/sign/SignUp.jsx
+++ src/pages/sign/SignUp.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { useForm } from 'react-hook-form';
 
 const Container = styled.div`
   width: 1200px;
   margin: 20vh auto;
-  min-height: 100vh;
   margin-bottom: 240px;
 `;
 
@@ -20,13 +19,17 @@ font-weight: bold;
 `;
 
 const Form = styled.form`
-width: 90%;
+width: 80%;
 margin: 0 auto;
 `;
 
 const Label = styled.label`
 display: flex;
+align-items: center;
 flex-direction: column;
+margin: 2rem 0;
+font-size: 0.8rem;
+color: #888;
 `;
 
 const Input = styled.input`
@@ -43,6 +46,12 @@ background: #fafafa;
 }
 `;
 
+const ErrorMessage = styled.div`
+font-size: 0.8rem;
+margin: 0.5rem 0 0 1rem;
+color: #888;
+`;
+
 const InputFile = styled.input``;
 
 const ButtonWrapper = styled.div`
@@ -53,12 +62,11 @@ const ButtonWrapper = styled.div`
 `;
 
 const Button = styled.button`
-  padding: 0.5rem 3rem;
+  padding: ${props => props.padding || '0.5rem 3rem'};
   border: none;
   box-shadow: 3px 4px 8px #b7b7b7;
   background: ${(props) => props.backgroundColor || "#416dea"};
   color: #fff;
-  font-weight: bold;
   border-radius: 30px;
   margin: 1rem 0;
   margin-left: ${(props) => props.marginLeft && "2rem"};
@@ -77,34 +85,89 @@ const Button = styled.button`
 
 function SignUp() {
 
-    const { register, handleSubmit, formState: { errors }, reset } = useForm();
+    const [user, setUser] = useState({});
+
+    const { register, handleSubmit, formState: { errors }, reset, getValues } = useForm();
+
+    const onSubmit = data => {
+        setUser({
+            name : data.name,
+            email: data.email,
+            password: data.password,
+            phone: data.phone
+        });
+        console.log(user);
+    };
+
+    const resetForm = () => {
+        reset({
+            name : '',
+            email: '',
+            password: '',
+            passwordConfirm: '',
+            phone: ''
+        })
+    }
 
     return ( 
         <Container>
             <FormWrapper>
                 <Title>SIgnUp</Title>
-                <Form>
+                <Form onSubmit={handleSubmit(onSubmit)}>
                     <Label>
-                        <Input placeholder='이름'></Input>
+                        <Input type='text' placeholder='이름' {...register('name', {
+                            required : true,
+                            // pattern : /^[A-Za-z0-9]{4,20}$/
+                        })}></Input>
+                        <ErrorMessage>
+                            {errors.name?.type === 'required' && '이름을 입력해주세요.'}
+                        </ErrorMessage>
                     </Label>
                     <Label>
-                        <Input placeholder='이메일'></Input>
+                        <Input type='text' placeholder='이메일' {...register('email', {
+                            required: true,
+                            // pattern: /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i
+                        })}></Input>
+                        <ErrorMessage>
+                            {errors.email?.type === 'required' && '이메일을 입력해주세요.'}
+                        </ErrorMessage>
                     </Label>
                     <Label>
-                        <Input placeholder='비밀번호'></Input>
+                        <Input type='password' placeholder='비밀번호' {...register('password', {
+                            required: true,
+                            // pattern: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/
+                        })}></Input>
+                        <ErrorMessage>
+                            {errors.password?.type === 'required' && '비밀번호를 입력해주세요.'}
+                        </ErrorMessage>
                     </Label>
                     <Label>
-                        <Input placeholder='비밀번호 확인'></Input>
+                        <Input type='password' placeholder='비밀번호 확인' {...register('passwordConfirm', {
+                            required: true,
+                            // pattern: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/
+                        })}></Input>
+                        <ErrorMessage>
+                            {errors.passwordConfirm?.type === 'required' && '비밀번호를 다시 입력해주세요.'}
+                            {getValues('password') !== getValues('passwordConfirm') && '비밀번호를 다시 확인해주세요.'}
+                        </ErrorMessage>
+                        <Button padding={'0.5rem 1.1rem'}>check</Button>
                     </Label>
-                    <Button>check</Button>
                     <Label>
-                        <Input placeholder='전화번호'></Input>
+                        <Input type='text' placeholder='전화번호' {...register('phone', {
+                            required: true,
+                            // pattern: /^01([0|1|6|7|8|9])-?([0-9]{3,4})-?([0-9]{4})$/
+                        })}></Input>
+                        <ErrorMessage>
+                            {errors.phone?.type === 'required' && '전화번호를 입력해주세요.'}
+                        </ErrorMessage>
                     </Label>
-                    {/* <Label>
-                        <InputFile></InputFile>
-                    </Label> */}
-                    <Button>가입</Button>
-                    <Button>reset</Button>
+                    <Label>
+                        <InputFile type='file' accept='image/*'></InputFile>
+                    </Label>
+                    <ButtonWrapper>
+                        <Button>가입</Button>
+                        <Button marginLeft onClick={resetForm}>reset</Button>
+                    </ButtonWrapper>
                 </Form>
             </FormWrapper>
         </Container>
