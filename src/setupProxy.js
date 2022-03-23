bd2e135bb8a958aeb06568afe5a001f40675eb69
emsthf <deunsol916@gmail.com>
2022-03-23 09:25:02 +0900

    connect AWS beanstalk

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,12 +1,12 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
-    app.use(
-        '/api',
-        createProxyMiddleware({
-            target : 'http://localhost:8080',
-            changeOrigin : true,
-        })
-    );
-        
+  app.use(
+    "/api",
+    createProxyMiddleware({
+      target:
+        "http://springbootgoal-env.eba-wzmejvgd.us-east-1.elasticbeanstalk.com/:8080",
+      changeOrigin: true,
+    })
+  );
 };
