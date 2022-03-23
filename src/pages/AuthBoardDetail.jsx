70fd296215405ef434110d831261ef433f638193
jsy11618 <jsy11618@gmail.com>
2022-03-23 17:00:57 +0900

    auth 2 pages color changed

--- src/pages/AuthBoardDetail.jsx
+++ src/pages/AuthBoardDetail.jsx
@@ -6,7 +6,7 @@ const Container = styled.div`
   width: 1280px;
   margin: 20vh auto;
   min-height: 100vh;
-  margin-bottom: 240px;
+  margin-bottom: 600px;
 `;
 
 const Wrapper = styled.div`
@@ -30,7 +30,7 @@ const AuthboardFrame = styled.form`
   width: 98%;
   height: 90vh;
   box-shadow: 4px 7px 10px #a6a6a6;
-  min-height: 10vh;
+  min-height: 90vh;
   display: flex;
   border-radius: 4%;
   text-align: center;
@@ -50,7 +50,7 @@ const GridBox116 = styled.div`
 `;
 
 const Writer = styled.button`
-  background-color: white;
+  background-color: #f7f6f6;
   width: 90%;
   border-radius: 7px;
   text-align: center;
@@ -110,7 +110,7 @@ const Title = styled.div`
 `;
 
 const TitleContent = styled.input`
-  background-color: #ebf7ff;
+  background-color: #f7f6f6;
   width: 98%;
   height: 65px;
   border-radius: 7px;
@@ -123,7 +123,7 @@ const TitleContent = styled.input`
 `;
 
 const Content = styled.input`
-  background-color: #ebf7ff;
+  background-color: #f7f6f6;
   width: 98%;
   height: 600px;
   border-radius: 7px;
