4871579d77236ff0ceda8037663d2fce49d89b5b
emsthf <deunsol916@gmail.com>
2022-03-30 19:38:45 +0900

    edit api url

--- src/pages/DashBoard.jsx
+++ src/pages/DashBoard.jsx
@@ -386,25 +386,17 @@ function DashBoard() {
 
   useEffect(() => {
     // 최근 진행중 목표 3개 불러오기
-    axios
-      .get(
-        "http://springbootgoal-env.eba-wzmejvgd.us-east-1.elasticbeanstalk.com/api/dGoal/0"
-      )
-      .then((Response) => {
-        setDoingGoals(Response.data);
-        console.log(Response.data);
-        setIsGoalPeriod(Response.data);
-      });
+    axios.get("/api/dGoal/0").then((Response) => {
+      setDoingGoals(Response.data);
+      console.log(Response.data);
+      setIsGoalPeriod(Response.data);
+    });
 
     // 최근 완료된 목표 3개 불러오기
-    axios
-      .get(
-        "http://springbootgoal-env.eba-wzmejvgd.us-east-1.elasticbeanstalk.com/api/dGoal/1"
-      )
-      .then((Response) => {
-        setDoneGoals(Response.data);
-        console.log(Response.data);
-      });
+    axios.get("/api/dGoal/1").then((Response) => {
+      setDoneGoals(Response.data);
+      console.log(Response.data);
+    });
   }, []);
 
   return (
